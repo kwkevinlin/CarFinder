c51d03e70a780625564d9adb226040b758828e5e
kwkevinlin <kwkevinlin@gmail.com>
2016-02-11 21:19:24 -0500

    Debug polyline renderer

--- WhereIsMyCar/ViewController.swift
+++ WhereIsMyCar/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreLocation
 import MapKit
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
     @IBOutlet weak var latitude: UILabel!
     @IBOutlet weak var longitude: UILabel!
@@ -23,14 +23,15 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     var locationManager: CLLocationManager = CLLocationManager()
     var mk = MKPointAnnotation()
-    var coorLat = 0.0, coorLong = 0.0
-    var resetBool = false
+    var coorLat = 0.0, coorLong = 0.0, carLat = 0.0, carLong = 0.0
+    var setLocBool = false, resetBool = false
+    var route: MKRoute?
     
     /*
         Note: Default will go to didFailWithError unless you specify location in simulator:
         iOS Simulator, Debug -> Location -> Custom Location
         36.136111, -80.279462 for Q parking lot
-        Use  7   for demonstration (36.137)
+        Use  74   for demonstration (36.1374)
     */
     
     override func viewDidLoad() {
@@ -42,6 +43,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         print("Button pressed!")
         print("Configuring CoreLocation...")
         resetBool = false
+        setLocBool = true
         
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.delegate = self
@@ -56,18 +58,24 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 
     
     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [CLLocation]!) { //AnyObject->CLLocation
-        let latestLocation: CLLocation = locations[locations.count - 1] //AnyObject
+        var latestLocation: CLLocation = locations[locations.count - 1] //AnyObject
         
-        print("Updating Location")
+        //print("Updating Location")
+        mapView.delegate = self
         
         coorLat = latestLocation.coordinate.latitude
         coorLong = latestLocation.coordinate.longitude
+        if (setLocBool == true) {
+            carLat = coorLat
+            carLong = coorLong
+            setLocBool = false
+        }
         latitude.text = String(format: "%.4f", coorLat)
         longitude.text = String(format: "%.4f", coorLong)
         altitude.text = String(format: "%.4f", latestLocation.altitude)
         
         //Get current location
-        let center = CLLocationCoordinate2D(latitude: coorLat, longitude: coorLong)
+        let carCoords = CLLocationCoordinate2D(latitude: carLat, longitude: carLong)
         
         if (resetBool == false) { //When reset is pressed, a different mapView sequence is ran
             
@@ -76,24 +84,38 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             */
             
             //Center is the center of where mapview will zone in on, span is the degree of zoom the map will be displayed in
-            let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.0035, longitudeDelta: 0.0035))
+            let region = MKCoordinateRegion(center: carCoords, span: MKCoordinateSpan(latitudeDelta: 0.0035, longitudeDelta: 0.0035))
             
             //mk declared as MKPointAnnotation above global. Adds annotation in current location (when set location button is pressed
-            mk.coordinate = CLLocationCoordinate2D(latitude: coorLat, longitude: coorLong)
+            mk.coordinate = carCoords
             mapView.addAnnotation(mk)
             
             //SetRegion sets "center" (the middle-point of where mapview will display) and span to determine how much zoom (how much map to show). Animated will animate the transition of locations instead of just frame A to frame B. showsUserLocation needs to be false here because it is turned on in "else" statement below (when resetButton is pressed, where user's location is continuously tracked).
             mapView.mapType = MKMapType.Hybrid
-            mapView.showsUserLocation = false
+            //mapView.showsUserLocation = false
             mapView.setRegion(region, animated: true)
             
-            locationManager.stopUpdatingLocation()
-            
-            /*
+            //Adding walking directions on map after "Set my Location" enabled
+            mapView.showsUserLocation = true
             var directionsRequest = MKDirectionsRequest()
-            let markTaipei = MKPlacemark(coordinate: CLLocationCoordinate2DMake(point1.coordinate.latitude, point1.coordinate.longitude), addressDictionary: nil)
-            let markChungli = MKPlacemark(coordinate: CLLocationCoordinate2DMake(point2.coordinate.latitude, point2.coordinate.longitude), addressDictionary: nil)
-            */
+            let carLocation = MKPlacemark(coordinate: carCoords, addressDictionary: nil) //center = current location
+            var currentLocation = MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: coorLat, longitude: coorLong), addressDictionary: nil)
+            
+            directionsRequest.source = MKMapItem(placemark: currentLocation)
+            directionsRequest.destination = MKMapItem(placemark: carLocation)
+            directionsRequest.transportType = MKDirectionsTransportType.Walking
+            
+            var directions = MKDirections(request: directionsRequest)
+            directions.calculateDirectionsWithCompletionHandler {
+                (response, error) -> Void in
+                if error == nil {
+                    self.route = response!.routes[0] as? MKRoute
+                    self.mapView.addOverlay((self.route?.polyline)!)
+                }
+            }
+            
+            //locationManager.stopUpdatingLocation()
+       
 
         } else {
             
@@ -109,7 +131,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             mapView.showsUserLocation = true
             
             //Refocus map to user's current location. Both "set my location" and "reset" button calls this function, thus the else statement and resetBool boolean
-            mapView.setRegion(MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.07, longitudeDelta: 0.07)), animated: true)
+            mapView.setRegion(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: coorLat, longitude: coorLong), span: MKCoordinateSpan(latitudeDelta: 0.07, longitudeDelta: 0.07)), animated: true)
             
             //locationManager.stopUpdatingLocation()
             
@@ -119,12 +141,22 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     }
     
+    func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
+        
+        print("RendererForOverLay")
+        
+        var myLineRenderer = MKPolylineRenderer(polyline: (route?.polyline)!)
+        myLineRenderer.strokeColor = UIColor.blueColor()
+        myLineRenderer.lineWidth = 3
+        return myLineRenderer
+    }
+    
     
     @IBAction func openInMaps(sender: AnyObject) {
         print("Openning in Maps")
         
         //Send coordinates and name to Apple Maps, then open
-        let placemark = MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: coorLat, longitude: coorLong), addressDictionary: nil)
+        let placemark = MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: carLat, longitude: carLong), addressDictionary: nil)
         let mapItem = MKMapItem(placemark: placemark)
         mapItem.name = "Your Parked Car"
         mapItem.openInMapsWithLaunchOptions(nil)
